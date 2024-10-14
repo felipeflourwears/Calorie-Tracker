c719c2a05fe1826246f65b4b724d7d043da8ef67
felipeflourwears <72992664+felipeflourwears@users.noreply.github.com>
2024-10-14 13:05:48 -0600

    UseState HandleChange Activity as Object

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react"
 import { categories } from "../data/categories"
 
 const Form = () => {
     
+    const [activity, setActivity] = useState({
+        category: 1,
+        name: '',
+        calories: 0
+    })
+
     
+    const handleChange = (e) => {
+        setActivity({
+            ...activity,
+            [e.target.id]: e.target.value
+        })
+    }
+
+
     return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="category" className="font-bold">Category:</label>
-            <select name="" id="category" className="border-slate-300 p-2 rounded-lig w-full bg-white">
+            <select name="" id="category" value={activity.category} onChange={handleChange} className="border-slate-300 p-2 rounded-lig w-full bg-white">
                 {categories.map(category=>(
                     <option 
                         key={category.id}
@@ -21,10 +36,12 @@ const Form = () => {
         <div className="grid grid-cols-1 gap-3">
             <label htmlFor="activity" className="font-bold">Activity:</label>
             <input 
-                id="activity"
+                id="name"
                 type="text"
+                value={activity.name}
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Ex. Food, Juice of Orange, Salad, Exercise, Weights, Bycyle" 
+                onChange={handleChange}
             />
         </div>
         <div className="grid grid-cols-1 gap-3">
@@ -32,6 +49,8 @@ const Form = () => {
             <input 
                 id="calories"
                 type="number"
+                onChange={handleChange}
+                value={activity.calories}
                 className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Calories Ex. 300 or 500" 
             />
